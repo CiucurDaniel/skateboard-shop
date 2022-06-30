49648a0ce06943852785e96421d720103768ca0c
Ciucur Daniel <ciucur.daniel14@gmail.com>
2022-06-30 14:23:00 +0300

    Parse templates html only once at package level

--- microservices/frontend/handlers.go
+++ microservices/frontend/handlers.go
@@ -7,11 +7,13 @@ import (
 	"text/template"
 )
 
-func (f *frontendServer) homeHandler(w http.ResponseWriter, r *http.Request){
+var templates = template.Must(template.New("").ParseGlob("templates/*.html"))
+
+func (f *frontendServer) homeHandler(w http.ResponseWriter, r *http.Request) {
 	log.Println("Home handler reached")
 
 	// Note: templates must not be loaded for every request, change in future
-	err := template.Must(template.New("").ParseGlob("templates/*.html")).ExecuteTemplate(w,"home", nil)
+	err := templates.ExecuteTemplate(w, "home", nil)
 	if err != nil {
 		fmt.Println("Error rendering page")
 		// todo: implement a renderHttpErrorPage
