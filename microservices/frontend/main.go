191b95f5b8a71a454d30e6b7adb98507d916b07e
Ciucur Daniel <ciucur.daniel14@gmail.com>
2022-06-30 14:23:50 +0300

    Change setup in main

--- microservices/frontend/main.go
+++ microservices/frontend/main.go
@@ -5,22 +5,50 @@ import (
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
+	"os"
+	"time"
 )
 
 type frontendServer struct {
-	// todo: here add endpoints to other microservices
-	// which will be configured with env variables
+	catalogMicroservice string
+	//authMicroservice    string
+	//shoppingCartMicroservice string
+	//orderTrackingMicroservice string
 }
 
+const port = 9000 // For the moment do not take port from env
 
-func main(){
-	svc := new(frontendServer)
+func main() {
 
 	fmt.Println("Welcome to Skateboard shop UI")
+
+	svc := new(frontendServer)
+	mustMapEnv(&svc.catalogMicroservice, "CATALOG_MICROSERVICE")
+	//mustMapEnv(&svc.authMicroservice, "AUTH_MICROSERVICE")
+	//mustMapEnv(&svc.shoppingCartMicroservice, "SHOPPING_CART_MICROSERVICE")
+	//mustMapEnv(&svc.orderTrackingMicroservice, "ORDER_TRACKING_MICROSERVICE")
 
 	r := mux.NewRouter()
 	r.HandleFunc("/", svc.homeHandler).Methods(http.MethodGet, http.MethodHead)
 
-	log.Println("Starting server on port 8080")
-	log.Fatal(http.ListenAndServe(":9000", r))
+	servAddr := fmt.Sprintf(":%v", port)
+	server := http.Server{
+		Addr:         servAddr,
+		Handler:      r,
+		ReadTimeout:  30 * time.Second,
+		WriteTimeout: 30 * time.Second,
+		IdleTimeout:  120 * time.Second,
+	}
+	log.Printf("starting frontend on %v", port)
+	log.Fatal(server.ListenAndServe())
+
+}
+
+func mustMapEnv(target *string, envKey string) {
+	v := os.Getenv(envKey)
+	if v == "" {
+		// Uncomment later, now we do not call those microservices
+		//panic(fmt.Sprintf("environment variable %q not set", envKey))
+	}
+	*target = v
 }
